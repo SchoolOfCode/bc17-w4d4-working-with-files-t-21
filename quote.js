31998cc235a6ee6fefb4e7434252e2f223d715e0
Mahik <mahik09@outlook.com>
2024-07-25 14:28:22 +0100

    ticket 2b and 2c completed

--- quote.js
+++ quote.js
@@ -1,38 +1,45 @@
 import fs from "node:fs/promises";
+import { parse } from "node:path";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
-export async function getQuotes(filePath) {
-try { 
-    const data = await readFile(filePath, 'utf8');
-    JSON.parse(data);
-    console.log(data);
-    } catch (error) {
-    console.error('Error reading file:', error);
-}
-};
-
-getQuotes("quotes.json")
-
-//not take in any arguments
-//read/parse all quote objects from quotes.json
-//return an array of all existing quote objects
-
-
-
-export async function addQuote(quoteText) {
-
+export async function getQuotes() {
+  try {
+    const data = await fs.readFile(fileName, "utf8");
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
 }
 
-export async function getRandomQuote() {
+getQuotes();
 
+export async function addQuote(quoteText1) {
+  const newQuote = { id: uuidv4(), quoteText: "placeholder" };
+  newQuote.quoteText = quoteText1;
+  console.log(newQuote);
+  const parsedRead = await getQuotes();
+  console.log(parsedRead);
+  parsedRead.push(newQuote);
+  console.log(parsedRead);
 }
+addQuote("this is soooo hard");
 
-export async function editQuote(id, quoteText) {
+// Inside `quote.js` find the `addQuote` function.
 
-}
+// The function should:
 
-export async function deleteQuote(id) {
+// - take in quoteText (String)
+// - create a quote object
+// - assign a unique id to the quote object (using a v4 uuid)
+// - read/parse all quote objects from `quotes.json`
+// - save the quote object to the `quotes.json` file at the end of the array
+// - return the newly created quote object
 
-}
+export async function getRandomQuote() {}
+
+export async function editQuote(id, quoteText) {}
+
+export async function deleteQuote(id) {}
