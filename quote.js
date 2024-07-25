2c62dc0bffd895f75b6eb0c3157a6af607f3bc8b
Sayed <sayed.sais@gmail.com>
2024-07-25 12:37:51 +0100

    attempted ticket 2b

--- quote.js
+++ quote.js
@@ -3,9 +3,23 @@ import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
 
-export async function getQuotes() {
-
+export async function getQuotes(filePath) {
+try { 
+    const data = await readFile(filePath, 'utf8');
+    JSON.parse(data);
+    console.log(data);
+    } catch (error) {
+    console.error('Error reading file:', error);
 }
+};
+
+getQuotes("quotes.json")
+
+//not take in any arguments
+//read/parse all quote objects from quotes.json
+//return an array of all existing quote objects
+
+
 
 export async function addQuote(quoteText) {
 
